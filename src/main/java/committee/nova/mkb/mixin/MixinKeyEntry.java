72a6511ad4ab18929fec2a15d12ce8468e1963ac
MikhailTapio <mikhailtapio13468@163.com>
2022-10-07 22:03:13 +0800

    Hopefully fixed keybinding color

--- src/main/java/committee/nova/mkb/mixin/MixinKeyEntry.java
+++ src/main/java/committee/nova/mkb/mixin/MixinKeyEntry.java
@@ -1,15 +1,18 @@
 package committee.nova.mkb.mixin;
 
 import committee.nova.mkb.api.IKeyBinding;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiKeyBindingList;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.EnumChatFormatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -27,14 +30,37 @@ public abstract class MixinKeyEntry {
     @Final
     private GuiButton btnChangeKeyBinding;
 
+    @Shadow(aliases = {"field_148284_a", "this$0"})
+    private GuiKeyBindingList outer;
+
     @Inject(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiButton;drawButton(Lnet/minecraft/client/Minecraft;II)V", ordinal = 0))
-    public void onDrawEntry1(int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci) {
+    public void onDrawEntry2(int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci) {
         btnReset.enabled = !((IKeyBinding) field_148282_b).isSetToDefaultValue();
     }
 
-    @Inject(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiButton;drawButton(Lnet/minecraft/client/Minecraft;II)V", ordinal = 1))
-    public void onDrawEntry2(int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci) {
+    @Redirect(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiButton;drawButton(Lnet/minecraft/client/Minecraft;II)V", ordinal = 1))
+    public void onDrawEntry3(GuiButton button, Minecraft mc, int i1, int i2) {
         btnChangeKeyBinding.displayString = ((IKeyBinding) field_148282_b).getDisplayName();
+        boolean conflicted = false;
+        boolean keyCodeModifierConflict = true; // less severe form of conflict, like SHIFT conflicting with SHIFT+G
+
+        if (this.field_148282_b.getKeyCode() != 0) {
+            for (KeyBinding keybinding : mc.gameSettings.keyBindings) {
+                final IKeyBinding mixined = (IKeyBinding) keybinding;
+                if (keybinding != this.field_148282_b && mixined.conflicts(this.field_148282_b)) {
+                    conflicted = true;
+                    keyCodeModifierConflict &= mixined.hasKeyCodeModifierConflict(this.field_148282_b);
+                }
+            }
+        }
+
+        if (((AccessorGuiBindingList) outer).getField_148191_k().buttonId == this.field_148282_b) {
+            this.btnChangeKeyBinding.displayString = EnumChatFormatting.WHITE + "> " + EnumChatFormatting.YELLOW + this.btnChangeKeyBinding.displayString + EnumChatFormatting.WHITE + " <";
+        } else if (conflicted) {
+            this.btnChangeKeyBinding.displayString = (keyCodeModifierConflict ? EnumChatFormatting.GOLD : EnumChatFormatting.RED) + this.btnChangeKeyBinding.displayString;
+        }
+
+        button.drawButton(mc, i1, i2);
     }
 
     @Inject(method = "mousePressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/GameSettings;setOptionKeyBinding(Lnet/minecraft/client/settings/KeyBinding;I)V"))
