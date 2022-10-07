fea30b719c6ca08cadba50ad7f019bd5a68fa23a
MikhailTapio <mikhailtapio13468@163.com>
2022-10-07 19:55:24 +0800

    Clean

--- src/main/java/committee/nova/mkb/mixin/MixinKeyEntry.java
+++ src/main/java/committee/nova/mkb/mixin/MixinKeyEntry.java
@@ -26,7 +26,6 @@ public abstract class MixinKeyEntry {
     @Shadow
     @Final
     private GuiButton btnChangeKeyBinding;
-    //private boolean keyCodeModifierConflict = false;
 
     @Inject(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiButton;drawButton(Lnet/minecraft/client/Minecraft;II)V", ordinal = 0))
     public void onDrawEntry1(int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci) {
@@ -42,32 +41,4 @@ public abstract class MixinKeyEntry {
     public void onMousePressed(int p_148278_1_, int p_148278_2_, int p_148278_3_, int p_148278_4_, int p_148278_5_, int p_148278_6_, CallbackInfoReturnable<Boolean> cir) {
         ((IKeyBinding) field_148282_b).setToDefault();
     }
-
-    //@Redirect(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;getKeyCode()I", ordinal = 4))
-    //public int trapDrawEntry(KeyBinding instance) {
-    //    return Integer.MIN_VALUE;
-    //}
-
-    //@Inject(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/settings/KeyBinding;getKeyCode()I", ordinal = 3), locals = LocalCapture.CAPTURE_FAILHARD)
-    //public void onDrawEntry1(
-    //        int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci,
-    //        boolean flag1, boolean flag2, KeyBinding[] akeybinding, int l1, int i2) {
-    //    keyCodeModifierConflict = true;
-    //    final int length = akeybinding.length;
-    //    for (int j = 0; j < length; ++j) {
-    //        KeyBinding binding = akeybinding[i2];
-    //        final IKeyBinding mixined = (IKeyBinding) binding;
-    //        if (binding != this.field_148282_b && mixined.conflicts(this.field_148282_b)) {
-    //            flag2 = true;
-    //            keyCodeModifierConflict &= mixined.hasKeyCodeModifierConflict(field_148282_b);
-    //        }
-    //    }
-    //}
-//
-    //@Inject(method = "drawEntry", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiButton;drawButton(Lnet/minecraft/client/Minecraft;II)V", ordinal = 1), locals = LocalCapture.CAPTURE_FAILHARD)
-    //public void onDrawEntry2(int p_148279_1_, int p_148279_2_, int p_148279_3_, int p_148279_4_, int p_148279_5_, Tessellator p_148279_6_, int p_148279_7_, int p_148279_8_, boolean p_148279_9_, CallbackInfo ci, boolean flag1, boolean flag2) {
-    //    if (flag1 || !flag2) return;
-    //    this.btnChangeKeyBinding.displayString = (keyCodeModifierConflict ? EnumChatFormatting.GOLD : EnumChatFormatting.RED) + this.btnChangeKeyBinding.displayString;
-    //}
-
 }
